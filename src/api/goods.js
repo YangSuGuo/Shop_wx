1d1e1dedfd0b4c93818210deea657e88e7b4d90e
YangSuGuo <3130876713@qq.com>
2024-05-16 11:21:21 +0800

    增加图片上传接口，修正uniapp图片上传逻辑

--- src/api/goods.js
+++ src/api/goods.js
@@ -1,5 +1,5 @@
 import http from '../common/http.js'
 //发布页面商品分类
 export const categoryApi = (parm)=>{
-    return http.get('/api/category/getSelectList',parm)
+    return http.get('/api/v1/category/getSelectList',parm)
 }
