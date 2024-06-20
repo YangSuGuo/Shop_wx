6e0fdecc9fec9390a7e8c592b0e6089b2312f3d5
YangSuGuo <3130876713@qq.com>
2024-06-20 16:25:54 +0800

    SpringBoot 同步更新 小程序更新密码 修改用户个人信息 查询用户个人信息 忘记密码 我发布的商品 修改发布的商品信息

--- src/api/order.js
+++ src/api/order.js
@@ -1,29 +1,29 @@
 import http from '../common/http.js'
 
-export const getUsedListApi = (parm)=>{
-	return http.post('/api/v1/goods/getUsedList',parm)
+export const getUsedListApi = (parm) => {
+    return http.post('/api/v1/goods/getUsedList', parm)
 }
 
-export const getBuyListApi = (parm)=>{
-	return http.post('/api/v1/goods/getBuyList',parm)
+export const getBuyListApi = (parm) => {
+    return http.post('/api/v1/goods/getBuyList', parm)
 }
 //查询所有分类
-export const getCateListApi =()=>{
-	return http.get('/api/v1/goods/getCateList')
+export const getCateListApi = () => {
+    return http.get('/api/v1/goods/getCateList')
 }
 //提交交易
-export const replaceOrderApi = (parm)=>{
-	return http.post('/api/v1/goods/replaceOrder')
+export const replaceOrderApi = (parm) => {
+    return http.post('/api/v1/goods/replaceOrder')
 }
 //查询我的求购订单
-export const getMyOrderApi = (parm)=>{
-	return http.post('/api/v1/goods/replaceOrder')
+export const getMyOrderApi = (parm) => {
+    return http.post('/api/v1/goods/replaceOrder')
 }
 //查询我的出售订单
-export const getSellOrderApi = (parm)=>{
-	return http.post('/api/v1/goods/replaceOrder')
+export const getSellOrderApi = (parm) => {
+    return http.post('/api/v1/goods/replaceOrder')
 }
 //删除
-export const deleteOrderApi = (parm)=>{
-	return http.post('/api/v1/goods/deleteOrder')
+export const deleteOrderApi = (parm) => {
+    return http.post('/api/v1/goods/deleteOrder')
 }
